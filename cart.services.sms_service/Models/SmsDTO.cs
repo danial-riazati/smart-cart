f6ffe8139611a967922cd0c8cc5cf54feaa5036b
danial riazati <dnr1380@gmail.com>
2022-10-06 12:24:54 +0330

    gitignore added

---
+++ cart.services.sms_service/Models/SmsDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace cart.services.sms_service.Models
+{
+    public class SmsDTO
+    {
+        public string MobileNumber { get; set; }
+        public int OtpCode { get; set; }
+    }
+}
+
---
+++ cart.services.sms_service/Program.cs
@@ -0,0 +1,48 @@
+﻿using cart.services.sms_service.Models;
+using Ghasedak;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var SMSToken = builder.Configuration["SMSToken"];
+var LineNumber = builder.Configuration["LineNumber"];
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.MapPost("/sendSms", async ([FromBody] SmsDTO dto) =>
+{
+try {
+    var sms = new Ghasedak.Core.Api(SMSToken);
+
+    var result = await sms.SendSMSAsync($"message\n code:{dto.OtpCode}", dto.MobileNumber, linenumber: LineNumber);
+    if (result.Result.Code == 200)
+    {
+        return Results.Ok();
+    }
+    else
+        return Results.Problem();
+
+} catch (Exception e)
+    {
+        return Results.Problem();
+    }
+
+}
+);
+
+
+app.Run();
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
