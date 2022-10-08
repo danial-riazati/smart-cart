0b863a0901d659ee4f004507c37011a998b7af5a
danial <dnr1380@gmail.com>
2022-10-08 10:43:26 +0330

    invoice service updated

--- cart.api_gateway/Program.cs
+++ cart.api_gateway/Program.cs
@@ -1,7 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json");
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
@@ -10,5 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseOcelot().Wait();
 
 app.Run();
